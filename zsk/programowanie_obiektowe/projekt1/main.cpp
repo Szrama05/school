9572f84adb8d1549c49356508a107b2d2e83d325
Szrama05 <90031943+Szrama05@users.noreply.github.com>
2021-09-10 10:49:37 +0200

    Add files via upload

--- zsk/programowanie_obiektowe/projekt1/main.cpp
+++ zsk/programowanie_obiektowe/projekt1/main.cpp
@@ -5,24 +5,50 @@ using namespace std;
 class Worker {
 	public:
 //deklaracja zmiennych cz³onkowskich (pól, w³aœciwoœci, cech)
-		string name;
-		string surname;
-		int age; //dokoñczyæ, sprawdziæ typ danych
+		string name, surname, nationality;
+		//unsigned short int age;
+		unsigned short int birthday;
+		float height;
+		char gender;
 		
 //definicja funkcji cz³onkowskiej (metody)
 		void showSurname() {
-			cout << "\nNazwisko pracownika: " << surname;
+			cout << "Nazwisko pracownika: " << surname << endl;
 		};
 		
 //deklaracja (prototyp) metody
+		void showName();
+		void showNameAndSur();
 		void showAllData();
 		
 };
 
-//definicja metody showAllData
-	void Worker::showAllData() {
-		cout << "\n\nImiê i nazwisko: " << name << " " << surname << endl;	
+//definicja metody showNameAndSur
+	void Worker::showNameAndSur() {
+		cout << "Imiê i nazwisko: " << name << " " << surname << endl;	
 	};
+	
+	void Worker::showName() {
+		cout << "Imiê: " << name << endl;	
+	};
+	
+	void Worker::showAllData() {
+		showNameAndSur();
+		cout << "Narodowoœæ: " << nationality << endl 
+			 << "Rok urodzenia: " << birthday << ", wzrost: " << height << "cm" << endl
+			 << "P³eæ: ";
+		switch(gender) {
+			case 'm': 
+				cout << "mê¿czyzna";
+				break;
+			case 'w': 
+				cout << "kobieta";
+				break;
+			default:
+				cout << "-";
+				break;
+		}
+	}
 	
 
 int main(int argc, char** argv) {
@@ -30,8 +56,13 @@ int main(int argc, char** argv) {
 	Worker pracownik1;
 	pracownik1.name = "Krystian";
 	pracownik1.surname = "Nowak";
-	cout << pracownik1.name;
-	pracownik1.showSurname();
+	pracownik1.birthday = 1995;
+	pracownik1.nationality = "Polska";
+	pracownik1.height = 175.34;
+	pracownik1.gender = 'm';
+	//cout << pracownik1.name;
+	//pracownik1.showSurname();
+	//pracownik1.showNameAndSur();
 	pracownik1.showAllData();
 	
 	return 0;
